212bd37248915fece0a4d8fbb19c7573ce9d1793
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-12-17 09:59:17 +0200

    OS X 10.6 SDK @ Xcode 3.2.1
    http://adcdownload.apple.com/Developer_Tools/xcode_3.2.1_developer_tools/xcode321_10m2003_developerdvd.dmg

--- MacOSX10.6.sdk/System/Library/Frameworks/Carbon.framework/Versions/A/Headers/Carbon.h
+++ MacOSX10.6.sdk/System/Library/Frameworks/Carbon.framework/Versions/A/Headers/Carbon.h
@@ -3,7 +3,7 @@
  
      Contains:   Master include for all of Carbon
  
-     Version:    Carbon-152~474
+     Version:    Carbon-152~475
  
      Copyright:  © 2000-2008 by Apple Computer, Inc., all rights reserved.
  
